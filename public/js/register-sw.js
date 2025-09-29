6d21005483ff11d7b3b03912f473aa39c1ebc57d
civil-soldier <krajeevranjan290@gmail.com>
2025-09-29 18:38:01 +0530

    pwa

--- public/js/register-sw.js
+++ public/js/register-sw.js
@@ -2,7 +2,7 @@
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/js/sw.js')
+    navigator.serviceWorker.register('/js/sw.js' , {scope: '/'})
       .then(registration => {
         console.log('Service Worker registered with scope:', registration.scope);
       })
